13ab124478247a9d4f1496c90f3ba54878ebdd96
alexanderDreher92 <39765744+alexanderDreher92@users.noreply.github.com>
2019-06-20 20:03:44 +0200

    Organizing
    Made the site and material package work
    started organizing:
    - index page for the first view
    - registration page for the new users
    - start page for thefirst view after login
    - transaction page for sending money

--- ProjectIndePay/src/app/app.module.ts
+++ ProjectIndePay/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+import { MatToolbarModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
