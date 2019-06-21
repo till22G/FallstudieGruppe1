8cd135fbe48c60d304fbd6b6a03ef3c56077dbb3
Tillman <tillman.galla@sas.com>
2019-06-21 11:00:18 +0200

    added shared folder and user model

---
+++ ProjectIndePay/src/app/shared/user.model.ts
@@ -0,0 +1,11 @@
+export class User {
+  public firstName: string;
+  public lastName: string;
+  public loginName: string;
+
+  constructor(firstName: string, lastName: string, loginName: string){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.loginName = loginName;
+  }
+}
