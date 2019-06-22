ff5cc752eded12a16131433c26e9c8f0ad2d2239
Tillman <tillman.galla@sas.com>
2019-06-22 23:57:51 +0200

    added form; submitting the form will now create a new object: registerUser

--- ProjectIndePay/src/app/startingPage/register/register.component.ts
+++ ProjectIndePay/src/app/startingPage/register/register.component.ts
@@ -1,4 +1,6 @@
 import { OnInit, Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { RegisterUser } from 'src/app/shared/register-user.model';
 
 @Component({
   selector: 'app-register',
@@ -7,13 +9,19 @@ import { OnInit, Component } from '@angular/core';
 })
 
 export class RegisterComponent implements OnInit {
+  user: RegisterUser;
 
   constructor() {}
 
   ngOnInit() {}
 
-  onRegister() {
-
-  }
+  onRegister(registerForm: NgForm) {
+    this.user = new RegisterUser(registerForm.value.firstName,
+                            registerForm.value.lastName,
+                            registerForm.value.loginName,
+                            registerForm.value.password,
+                            registerForm.value.passwordRepeat);
+    console.log(this.user.password);
+                          }
 
 }
