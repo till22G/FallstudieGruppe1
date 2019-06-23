5f983b34d11da1fca31990348247c8a145d19b6d
Tillman <tillman.galla@sas.com>
2019-06-23 21:27:04 +0200

    changed sessionUser to authentication-data + implemented authentication service

---
+++ ProjectIndePay/src/app/startingPage/authentication.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AuthenticationData } from '../shared/AuthenticationData';
+
+@Injectable({ providedIn: 'root'})
+export class AuthenticationService {
+  constructor(private http: HttpClient) {}
+
+  login(loginName: string, password: string) {
+    // create const with the data passed to the login method of the service
+    const authenticationData: AuthenticationData ={loginName: loginName, password: password};
+    // pass authenticationData to http and post it + subsribe for response
+    this.http.post("", authenticationData)
+      .subscribe(response => {
+        console.log("auth worked");
+        //implement what schoul happen
+      });
+  }
+
+
+  //implement registration method here
+}
