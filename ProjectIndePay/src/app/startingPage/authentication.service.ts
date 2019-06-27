6fd09fa2dc7ca1335a35f7a29201971d81124bfd
Tillman <tillman.galla@sas.com>
2019-06-27 23:49:41 +0200

    added home page and redirection to homepage after login + added logout and redirection to ifo page after logout

--- ProjectIndePay/src/app/startingPage/authentication.service.ts
+++ ProjectIndePay/src/app/startingPage/authentication.service.ts
@@ -2,12 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthenticationData } from '../shared/authentication-data';
 import { RegisterUser } from '../shared/register-user.model';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root'})
 export class AuthenticationService {
   private token: string;
+  private userLoginName: string;
+  private authenticationStatusListener = new Subject<boolean>();
+  private authenticationNameListener = new Subject<string>();
+  private isAuthenticated = false;
 
-  constructor(private http: HttpClient) {}
+
+  constructor(private http: HttpClient, private router: Router) {}
+
 
 
   login(loginName: string, password: string) {
@@ -15,16 +23,35 @@ export class AuthenticationService {
     const authenticationData: AuthenticationData = {loginName: loginName, password: password};
     // pass authenticationData to http and post it + subsribe for response
 
-    this.http.post('http://localhost:3000/control/users/read', authenticationData)
+    this.http.post<{jwt: string, firstName: string}>('http://localhost:3000/control/users/read', authenticationData)
     .subscribe(response => {
       console.log('auth worked');
       console.log(response);
-      // get token from response
-      // const token = response.token;
-      // this.token = token;
-      // implement what schoul happen
+
+      // get token from response, store it in private token attribute and set up observable
+      const token = response.jwt;
+      this.token = token;
+      console.log(this.token);
+
+      //check if a token was returned
+      if(token) {
+        // set is authenticated to true
+        this.isAuthenticated = true;
+
+        // set the authenticationStatus to true
+        this.authenticationStatusListener.next(true);
+
+        // get userName from response, store it in private userLoginName and set up observable
+        const userLoginName = response.firstName;
+        this.userLoginName = userLoginName;
+        this.authenticationNameListener.next(this.userLoginName);
+
+        // call router an navigate to the home page
+        this.router.navigate(['/home']);
+      }
     });
   }
+
 
 
   // implement registration method here
@@ -39,13 +66,41 @@ export class AuthenticationService {
       repeatPassword : repeatPassword};
 
       // implement path for user registration
-    this.http.post('', RegisterUser).subscribe(response => {
+    this.http.post('http://localhost:3000/control/users/create', registerUser).subscribe(response => {
         console.log('registration worked');
         console.log(response);
       });
     }
 
+
+
+    // takes care of the stepps to clear token and inform the application about the logout
+    logout() {
+      // set token to null to clear it
+      this.token = null;
+      // sets is Authenticated to false
+      this.isAuthenticated = false;
+      // inform subscribing roles that user is not longer authenticated
+      this.authenticationStatusListener.next(false);
+      // call rouer and navigate back to the info-page
+      this.router.navigate(['/info']);
+    }
+
+
+
     getToken() {
       return this.token;
+    }
+
+    getAuthenticationStatusListener() {
+      return this.authenticationStatusListener.asObservable();
+    }
+
+    getAuthenticationNameListener() {
+      return this.authenticationNameListener.asObservable();
+    }
+
+    getIsAuthenticated() {
+      return this.isAuthenticated;
     }
   }
